0659fdee9e44868bfe8c77c42267d62f4fbaac66
Sesmose <arnaudpadula5@gmail.com>
2020-10-15 18:52:05 +0200

    sccqscqs
    cscsc

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        description: 'Beautiful socks',
+        image: './assets/images/socks_green.jpg'
+}
+})
